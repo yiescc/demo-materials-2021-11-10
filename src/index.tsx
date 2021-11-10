e200a40371c13a29b2f88114fbae9d0c2422e935
yichao <56287527@qq.com>
2021-11-10 15:31:24 +0800

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import styles from './index.module.scss';
+
+export default function ExampleComponent() {
+  return <div className={styles.Container}>Hello ExampleComponent</div>;
+}
