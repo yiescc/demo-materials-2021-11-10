56498088f2330ca5188cabd44b966d314d542796
yichao <56287527@qq.com>
2021-11-10 16:03:25 +0800

    feat: add es and lib

---
+++ es/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import styles from './index.module.scss';
+export default function ExampleComponent() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: styles.Container
+  }, "Hello ExampleComponent");
+}
